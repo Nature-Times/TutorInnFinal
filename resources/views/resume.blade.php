0eda145225f5142d85151730fcdf1ba213f74162
Nature-Times <122514986+Nature-Times@users.noreply.github.com>
2025-01-03 21:08:55 +0700

    Update resume.blade.php

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -5,6 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tutor Profile</title>
     <link rel="stylesheet" href="css/resume.css">
+    <style>
+        .tutorBox {
+            align-items: center;
+            display: block;
+            margin-bottom: 4%;
+            padding-bottom: 200px;
+        }
+        
+        .button{
+            padding-left: 200px;
+            padding-top: 200px;
+        }
+        
+        .cv {
+            padding-top: 5%;
+        }
+        .userProfile {
+            border-radius: 50px;
+        }
+        a {
+            text-decoration: none;
+            color: black;
+        }
+        #logout {
+            color: black;
+            border: none;
+            font-family: 'Kameron';
+            font-size: 1.5vw;
+        }
+        #logout:hover {
+            text-decoration: underline;
+            transition: 3s;
+        }
+    </style>
 </head>
 
 <body>
